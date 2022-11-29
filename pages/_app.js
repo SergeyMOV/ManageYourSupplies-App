5429cdc14416bed34bad57a33442f2f4f289d20f
Sergey Movsesyan <sm877141@gmail.com>
2022-11-29 10:45:20 +0100

    added Prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import GlobalStyles from "../styles/GlobalStyles";
 import Header from "../components/Header";
 
-
 function MyApp({Component, pageProps}) {
   return (
     <>
       <GlobalStyles />
       <Header />
       <Component {...pageProps} />
-      
     </>
   );
 }

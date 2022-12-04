641e171051334948583bc0244c28cad640469552
Sergey Movsesyan <sm877141@gmail.com>
2022-12-04 20:02:56 +0100

    Added functionality from Input to Progressbar

--- pages/Page2.js
+++
@@ -1,3 +0,0 @@
-export default function Expenditure() {
-  return <h1>Hello World</h1>;
-}
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,31 @@ import Progress from "../components/ProgressBar";
 import Valuefields from "../components/In&OutputFields";
 import Navmenu from "../components/Navbar";
 import Valueinfo from "../components/Valueinfofields";
+import Header from "../components/Header";
+import {useState} from "react";
+
 export default function Home() {
+  const [budget, setBudget] = useState(0);
+  const [percentage,setPercentage] = useState(0);
+  const [progress, setProgress] = useState(0);
+  function handleChangeBudget(newBudget) {
+    setBudget(newBudget);
+  }
+  function handlePrecent(newPercentage){
+    setPercentage(newPercentage);
+  }
+  function handleProgress(newProgress){
+    setProgress(newProgress);
+  }
+  
   return (
     <>
-      <Entryfield />
+      <Header />
+      <Entryfield progress={progress} budget={budget} percentage={percentage} onChangeBudget={handleChangeBudget} onChangePercent={handlePrecent} onChangeProgress={handleProgress}  />
       <Navmenu />
       <Valuefields />
-      <Progress />
-      <Valueinfo />
+      <Progress progress={progress} percentage={percentage} budget={budget} />
+      <Valueinfo  budget={budget}/>
     </>
   );
 }

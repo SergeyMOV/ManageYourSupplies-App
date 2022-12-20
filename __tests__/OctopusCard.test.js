f2864e55ff8386422a20da15c93c6d06d640cab0
Sergey Movsesyan <sm877141@gmail.com>
2022-12-20 13:34:38 +0100

    fixed test issues

---
+++ __tests__/OctopusCard.test.js
@@ -0,0 +1,13 @@
+import {render, screen} from "@testing-library/react";
+import Valueinfo from "../components/Valueinfofields.js";
+import "@testing-library/jest-dom";
+
+describe("Valueiinfo", () => {
+  it("renders an octopus", () => {
+    render(<Valueinfo/>);
+
+    const heading = screen.getByTestId("test");
+
+    expect(heading).toBeInTheDocument();
+  });
+});
